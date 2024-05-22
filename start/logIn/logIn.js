7a79735a731f3110879a4c5ff959dc9a96f2be44
whoisluci <lucmov99@gmail.com>
2024-05-22 13:39:32 +0200

    Revert "Updated login" and updated login
    This reverts commit e8836a8435fad3d908679f679250be118db819db.

--- start/logIn/logIn.js
+++ start/logIn/logIn.js
@@ -2,47 +2,30 @@ function renderLogIn (parentID) {
     const DOM = document.getElementById(parentID);
     DOM.innerHTML = `
     <div id="top">
-        <img id="logoImg" src="../../media/icons/logo.svg">
-        <h1 id="logoFont">goodFilms</h1>
+        <img id="logo" src="">
     </div>
-    <hr>
-    <div id="bottom">
-        <input type="text" name="username" placeholder="username" id="username">
-        <input type="password" name="password" placeholder="password" id="password">
+    <div id="bottom>
+        <input type="text" name="name" placeholder="name">
+        <input type="password" name="password" placeholder="password">
 
         <button id="logInBttn">LOG IN</button>
-        <h3>Not a memeber? <a id="registerLink" href="../register">Register</a></h3>
+        <h3>Not a memeber? <a id="registerLink" href="">Register</a></h3>
     </div>
-    <hr>
     `;
 
-    // const registerBttn = document.getElementById("registerBttn");
-    // registerBttn.addEventListener("click", () => {
-    //     // omdirigera till registersidan
-    // })
+    const registerBttn = document.getElementById("registerBttn");
+    registerBttn.addEventListener("click", () => {
+        // omdirigera till registersidan
+    })
 
-    const logInBttn = document.getElementById("logInBttn");
-    logInBttn.addEventListener("click", async () => {
-        let username = await document.getElementById("username").value;
-        let password = await document.getElementById("password").value;
+    const logInBttn = document.getElementById("logInLink");
+    logInBttn.addEventListener("click", () => {
+        let username = document.getElementById("username").value;
+        let password = document.getElementById("password").value;
 
         if (username && password) {
-            console.log("hej");
-            let userInfo = {
-                username: username,
-                password: password,
-            };
-    
-            let logInRqst = new Request(`api/login.php`, {
-                method: "POST",
-                headers: {"Content-Type": "application/json"},
-                body: JSON.stringify(userInfo)
-            });
-    
-            let logInFetch = await fetcher(logInRqst);
-            let logInToken = await logInFetch.token;
+            
         }
     });
+    
 }
-
-renderLogIn("wrapper");
